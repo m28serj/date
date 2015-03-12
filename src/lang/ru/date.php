5cee6b18d45fb57a4c6228b209af05798aca177a
m28serj <serj.m28@gmail.com>
2015-03-12 18:37:11 +0200

    First Commit

---
+++ src/lang/ru/date.php
@@ -0,0 +1,61 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => ':time назад',
+    'from_now'  => 'через :time',
+    'after'     => ':time после',
+    'before'    => ':time до',
+    'year'      => ':count год|:count года|:count лет',
+    'month'     => ':count месяц|:count месяца|:count месяцев',
+    'week'      => ':count неделю|:count недели|:count недель',
+    'day'       => ':count день|:count дня|:count дней',
+    'hour'      => ':count час|:count часа|:count часов',
+    'minute'    => ':count минуту|:count минуты|:count минут',
+    'second'    => ':count секунду|:count секунды|:count секунд',
+
+		'january'   => 'январь',
+		'february'  => 'февраль',
+		'march'     => 'март',
+		'april'     => 'апрель',
+		'may'       => 'май',
+		'june'      => 'июнь',
+		'july'      => 'июль',
+		'august'    => 'август',
+		'september' => 'сентябрь',
+		'october'   => 'октябрь',
+		'november'  => 'ноябрь',
+		'december'  => 'декабрь',
+
+		'fjanuary'   => 'января',
+		'ffebruary'  => 'февраля',
+		'fmarch'     => 'марта',
+		'fapril'     => 'апреля',
+		'fmay'       => 'мая',
+		'fjune'      => 'июня',
+		'fjuly'      => 'июля',
+		'faugust'    => 'авгуса',
+		'fseptember' => 'сентября',
+		'foctober'   => 'октября',
+		'fnovember'  => 'ноября',
+		'fdecember'  => 'декабря',
+
+    'monday'    => 'понедельник',
+    'tuesday'   => 'вторник',
+    'wednesday' => 'среда',
+    'thursday'  => 'четверг',
+    'friday'    => 'пятница',
+    'saturday'  => 'суббота',
+    'sunday'    => 'воскресенье',
+
+);
---
+++ src/lang/uk/date.php
@@ -0,0 +1,61 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => ':time назад',
+    'from_now'  => 'через :time',
+    'after'     => ':time після',
+    'before'    => ':time до',
+    'year'      => ':count рік|:count роки|:count років',
+    'month'     => ':count місяць|:count місяці|:count місяців',
+    'week'      => ':count тиждень|:count тижні|:count тижнів',
+    'day'       => ':count день|:count дні|:count днів',
+    'hour'      => ':count година|:count години|:count годин',
+    'minute'    => ':count хвилину|:count хвилини|:count хвилин',
+    'second'    => ':count секунду|:count секунди|:count секунд',
+
+		'january'   => 'січень',
+		'february'  => 'лютий',
+		'march'     => 'березень',
+		'april'     => 'квітень',
+		'may'       => 'травень',
+		'june'      => 'червень',
+		'july'      => 'липень',
+		'august'    => 'серпень',
+		'september' => 'вересень',
+		'october'   => 'жовтень',
+		'november'  => 'листопад',
+		'december'  => 'грудень',
+
+		'fjanuary'   => 'січня',
+		'ffebruary'  => 'лютого',
+		'fmarch'     => 'березня',
+		'fapril'     => 'квітня',
+		'fmay'       => 'травня',
+		'fjune'      => 'червня',
+		'fjuly'      => 'липня',
+		'faugust'    => 'серпня',
+		'fseptember' => 'вересня',
+		'foctober'   => 'жовтня',
+		'fnovember'  => 'листопаду',
+		'fdecember'  => 'грудня',
+
+    'monday'    => 'понедiлок',
+    'tuesday'   => 'вiвторок',
+    'wednesday' => 'середа',
+    'thursday'  => 'четвер',
+    'friday'    => 'п\'ятниця',
+    'saturday'  => 'субота',
+    'sunday'    => 'неділя',
+
+);
